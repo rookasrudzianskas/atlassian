02852a59f363af25e1e7f9927e22379e6994ec6e
rokas <rokas@rokas.com>
2024-07-09 16:23:57 +0300

    Add copy button to allow users to copy chatbot link

--- app/(admin)/edit-chatbot/[id]/page.tsx
+++ app/(admin)/edit-chatbot/[id]/page.tsx
@@ -4,6 +4,8 @@ import React, {useEffect, useState} from 'react';
 import Link from "next/link";
 import {Input} from "@/components/ui/input";
 import {BASE_URL} from "@/graphql/apolloClient";
+import {Button} from "@/components/ui/button";
+import {Copy} from "lucide-react";
 
 const EditChatbot = ({params: {id}}: { params: { id: string }}) => {
   const [url, setUrl] = useState<string>('');
@@ -21,10 +23,21 @@ const EditChatbot = ({params: {id}}: { params: { id: string }}) => {
         <p className={'text-sm italic text-white'}>
           Share this link with your customers to start chatting with your chatbot.
         </p>
-        <div>
+        <div className={'flex items-center space-x-2'}>
           <Link href={url} className={'w-full cursor-pointer hover:opacity-50'}>
             <Input value={url} readOnly={true} className={'cursor-pointer'}/>
           </Link>
+          <Button
+            size={'sm'}
+            className={'px-3'}
+            onClick={() => {
+              navigator.clipboard.writeText(url);
+              // toast.success('Copied to clipboard!');
+            }}
+          >
+            <span className={'sr-only'}>Copy</span>
+            <Copy className={'w-4 h-4'} />
+          </Button>
         </div>
       </div>
     </div>
