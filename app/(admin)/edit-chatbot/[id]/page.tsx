3dd8070f20c1a49e7e944a1a80d89d532a710dd7
rokas <rokas@rokas.com>
2024-07-09 16:20:53 +0300

    Add client-side functionality to EditChatbot page

--- app/(admin)/edit-chatbot/[id]/page.tsx
+++ app/(admin)/edit-chatbot/[id]/page.tsx
@@ -1,9 +1,32 @@
-import React from 'react';
+"use client";
 
-const EditChatbot = ({}) => {
+import React, {useEffect, useState} from 'react';
+import Link from "next/link";
+import {Input} from "@/components/ui/input";
+import {BASE_URL} from "@/graphql/apolloClient";
+
+const EditChatbot = ({params: {id}}: { params: { id: string }}) => {
+  const [url, setUrl] = useState<string>('');
+
+  useEffect(() => {
+    const url = `${BASE_URL}/chatbot/${id}`;
+    setUrl(url);
+  }, [id]);
+
   return (
-    <div>
-      
+    <div className={'px-0 md:p-10'}>
+      <div className={'md:sticky md:top-0 z-50 sm:max-w-sm ml-auto space-y-2 md:border p-5 rounded-b-lg' +
+        ' md:rounded-lg bg-[#2991EE]'}>
+        <h2 className={'text-sm text-white font-bold'}>Link to Chat</h2>
+        <p className={'text-sm italic text-white'}>
+          Share this link with your customers to start chatting with your chatbot.
+        </p>
+        <div>
+          <Link href={url} className={'w-full cursor-pointer hover:opacity-50'}>
+            <Input value={url} readOnly={true} className={'cursor-pointer'}/>
+          </Link>
+        </div>
+      </div>
     </div>
   );
 };
