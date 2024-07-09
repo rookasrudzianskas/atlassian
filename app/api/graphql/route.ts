89c4278353f182d61d0bc8a04fe72f42b2ca920c
rokas <rokas@rokas.com>
2024-07-09 12:49:42 +0300

    Refactor GraphQL route POST function to handle mutation

--- app/api/graphql/route.ts
+++ app/api/graphql/route.ts
@@ -1,13 +1,30 @@
 import {NextRequest} from "next/server";
+import {serverClient} from "@/lib/server/serverClient";
+import {gql} from "@apollo/client";
 export async function POST(request: NextRequest) {
   const {query, variables} = await request.json();
 
   try {
+    let result;
     if (query.trim().startsWith("mutation")) {
-
+      result = await serverClient.mutate({
+        mutation: gql`
+        ${query}
+        `,
+        variables: variables,
+      })
     } else {
-
+      result = await serverClient.query({
+        query: gql`
+        ${query}
+        `,
+        variables: variables,
+      });
     }
+
+    const data = result.data;
+    console.log(">>> DATA", data);
+
   } catch (error) {
     console.error(error);
   }
