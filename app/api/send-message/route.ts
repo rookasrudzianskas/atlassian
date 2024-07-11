a2c0e50480550381629f4e18ad05e1baee4f58db
rokas <rokas@rokas.com>
2024-07-11 16:32:46 +0300

    Add system prompt message to chat completion messages

--- app/api/send-message/route.ts
+++ app/api/send-message/route.ts
@@ -46,6 +46,30 @@ export async function POST(req: Request) {
       content: message.content,
     }));
 
+    const systemPrompt = chatbot.chatbot_characteristics.map((c) => c.content).json(" + ");
+    console.log("SYSTEM PROMPT", systemPrompt);
+
+    const messages: ChatCompletionMessageParam[] = [
+      {
+        role: "system",
+        name: "system",
+        content: `You are a helpful assistant talking to ${name}. If a generic queston is asked, please answer it with the following system prompt: ${systemPrompt}. Please answer the question in the same language as the system prompt. If the question is not related to the system prompt, please answer with "I'm sorry, I don't know the answer to that question." If the question is not related to the system prompt, please answer with "I'm sorry, I don't know the answer to that question." `,
+      },
+      ...formattedPreviousMessages,
+      {
+        role: "user",
+        name: name,
+        content: content,
+      }
+    ];
+
+    const openaiResponse = await openai.chat.completions.create({
+      messages: messages,
+      model: 'gpt-4o',
+    });
+
+    console.log("OPENAI RESPONSE", openaiResponse);
+
   } catch (error) {
     console.error('This is error', error);
     return NextResponse.json({
